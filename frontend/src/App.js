46b334669c364500302cc5b1fad12381079bed9b
vikumR <vikumravishanka@gmail.com>
2023-04-29 13:14:07 +0530

    added styled navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Navigate to="/auth" />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/bookings" element={<BookingsPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+
+
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className='main-content'>
+            <Routes>
+              <Route index path="/" element={<Navigate to="/auth" />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/events" element={<EventsPage />} />
+              <Route path="/bookings" element={<BookingsPage />} />
+            </Routes>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
